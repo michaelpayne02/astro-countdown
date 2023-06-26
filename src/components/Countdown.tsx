12b9da08b721a82db5ad265394eb179583015a91
Michael Payne <mikeapayne02@gmail.com>
2023-06-25 22:39:32 -0500

    fix date

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,19 +1,22 @@
 import { createSignal } from 'solid-js';
 
 const Countdown = (props) => {
-  const [until, setUntil] = createSignal(0);
-  setUntil(props.targetDate.getTime() - new Date().getTime());
+  const [remaining, setRemaining] = createSignal(0);
+  setRemaining(props.targetDate.getTime() - new Date().getTime());
 
-  const days = () => Math.floor(until() / (1000 * 60 * 60 * 24));
+  const days = () => Math.floor(remaining() / (1000 * 60 * 60 * 24));
   const hours = () =>
-    Math.floor((until() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = () => Math.floor((until() % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = () => Math.floor((until() % (1000 * 60)) / 1000);
+    Math.floor((remaining() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = () => Math.floor((remaining() % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = () => Math.floor((remaining() % (1000 * 60)) / 1000);
 
-  setInterval(
-    () => setUntil(props.targetDate.getTime() - new Date().getTime()),
-    1000
-  );
+  setTimeout(() =>{
+    setInterval(
+      () => setRemaining(props.targetDate.getTime() - new Date().getTime()),
+      1000
+    );
+  }, 1000 - new Date().getMilliseconds())
+  
 
   return (
     <div class="pt-10 h-screen bg-gradient-to-b from-blue-50 to-sky-300 dark:from-cyan-700 dark:to-sky-900">
