f2d50bb2459dad067b3b00466fe3d792be1cffae
Michael Payne <mikeapayne02@gmail.com>
2023-06-25 21:54:00 -0500

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  output: "server",
+  adapter: vercel()
 });
